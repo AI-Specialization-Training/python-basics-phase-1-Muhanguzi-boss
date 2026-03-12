ab4896773c3ac0a7f2d9acba3700b16a67fcb787
Muhanguzi boss <bossmuhanguzi@gmail.com>
2026-03-12 19:44:57 +0200

    Solution to exerce 2

--- exercise-2.py
+++ exercise-2.py
@@ -1,4 +1,6 @@
 # ============================================================================
 # TODO: Write a greet function. It takes one argument, a name, and returns Hello, Name!. Use a formatted string.
 # ============================================================================
-
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("Muhanguzi"))
